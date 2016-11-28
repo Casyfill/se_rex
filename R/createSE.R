80423bdbe0560c8d1d88900c1cd799bfa7e5e749
Philipp Kats <philippk@philippk1.zillow.local>
2016-11-27 19:49:34 -0500

    minor

--- R/createSE.R
+++ R/createSE.R
@@ -1,6 +1,5 @@
-#' creates new workbook, registers styles
-#' and adds given sheets
-#' NOTE: maybe should return wb only, then get sheets through getSheets()
+#' @description  creates new workbook, registers styles
+#' and adds given sheets NOTE: maybe should return wb only, then get sheets through getSheets()'
 #' @param sheetList: named list, list(SheetName='Sheet title')
 #' @return named list of WB (workbook) and Sheets (named list of sheets)
 #' @return sheets: named list of registered sheets
