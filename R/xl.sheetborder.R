0576818271bca7b4a354a1239dd9ea4c40479965
Philipp Kats <casyfill@gmail.com>
2016-12-06 14:52:05 -0500

    corners try2

--- R/xl.sheetborder.R
+++ R/xl.sheetborder.R
@@ -62,7 +62,7 @@ xl.sheetborder <- function(sheet, width, height, rowStart, colStart){
     mydummy <-xlsx::setCellStyle(cell[[1]], styles[['borders']][['ur_corner']])
 
     cell <-xlsx::getCells(getRows(sheet, rowIndex=(rowStart+height)), colIndex=colStart)
-    mydummy <-xlsx::setCellStyle(cell[[1]], styles[['borders']][['ur_corner']])
+    mydummy <-xlsx::setCellStyle(cell[[1]], styles[['borders']][['ul_corner']])
 
     cell <-xlsx::getCells(getRows(sheet, rowIndex=(rowStart+height)), colIndex=(colStart+width))
     mydummy <-xlsx::setCellStyle(cell[[1]], styles[['borders']][['ur_corner']])
