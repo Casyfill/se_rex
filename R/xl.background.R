32c48806a8ac239fa1195a140e18399acaec884e
Philipp Kats <casyfill@gmail.com>
2016-12-06 14:06:35 -0500

    larger background area

--- R/xl.background.R
+++ R/xl.background.R
@@ -5,8 +5,8 @@
 #' @family internal
 xl.background <- function(sheet, background_style){
   #' create a background color fill
-  rows <- xlsx::createRow(sheet,rowIndex=1:100)
-  cells <- xlsx::createCell(rows, colIndex=1:100)
+  rows <- xlsx::createRow(sheet,rowIndex=1:1000)
+  cells <- xlsx::createCell(rows, colIndex=1:1000)
 
   wrap <- function(cell){
     xlsx::setCellStyle(cell, background_style)
